1727c2502cd4c2060d88a85b0d539e41bb08f033
yassinenafaa <yassinenafaa37@gmail.com>
2024-09-16 10:08:17 +0100

    triinser

---
+++ tri inser .js
@@ -0,0 +1,11 @@
+function insertionSort(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        let current = arr[i];
+        let j = i - 1;
+
+        while (j >= 0 && arr[j] > current) {
+            arr[j + 1] = arr[j];
+            j--; s
+        }
+        arr[j + 1] = current;
+    }
